9456d2d2c1828775449d629a2e43ebe452cb27ec
kayky <kaykygabriel31@gmail.com>
2025-08-21 15:51:58 -0300

    1° aula

---
+++ biblioteca/src/Livro.java
@@ -0,0 +1,58 @@
+public class Livro
+{
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String titulo, String autor, int ano) {
+        setTitulo(titulo);
+        setAutor(autor);
+        setAno(ano);
+
+    }
+
+    public String getano() {
+        return titulo;
+    }
+    public void setAno(int ano) {
+        int ano_atual = 2025;
+        if (ano>ano_atual) {
+            System.out.println("ERRO: ano invalido.");
+        }else {
+            this.ano = ano;
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        if (titulo == "") {
+            System.out.println("ERRO: titulo invalido.");
+        }else{
+                this.titulo = titulo;
+            }
+
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        if (autor == "") {
+            System.out.println("ERRO: autor invalido.");
+        }else{
+            this.autor = autor;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                '}';
+    }
+}
