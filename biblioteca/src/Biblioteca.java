231036e0b60afd6d0980c01fea7761c07e2bb51d
kayky <kaykygabriel31@gmail.com>
2025-08-21 15:52:38 -0300

    1° aula

---
+++ biblioteca/src/Biblioteca.java
@@ -0,0 +1,11 @@
+
+public class Biblioteca{
+    public static void main(String[] args){
+        Livro livro1 = new Livro("LPII","Thiago",2025);
+        livro1.setAno(2027);
+        livro1.setTitulo("");
+        livro1.setAutor("kinha");
+        System.out.println(livro1);
+    }
+
+}
